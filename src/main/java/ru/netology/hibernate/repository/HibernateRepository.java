c286aca9f10221516f7151dc28eac6b1f306b211
Kiva Aleksey <adolfp@mail.ru>
2021-10-28 14:31:31 +0700

    Changed findByCityOfLiving for query

--- src/main/java/ru/netology/hibernate/repository/HibernateRepository.java
+++ src/main/java/ru/netology/hibernate/repository/HibernateRepository.java
@@ -1,7 +1,9 @@
 package ru.netology.hibernate.repository;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.netology.hibernate.entity.Persons;
 import ru.netology.hibernate.entity.PrimaryKeyForPersons;
@@ -12,7 +14,8 @@ import java.util.Optional;
 @Repository
 public interface HibernateRepository extends CrudRepository<Persons, PrimaryKeyForPersons> {
 
-    List<Persons> findByCityOfLiving(String city);
+    @Query("select p from Persons p where p.cityOfLiving = :city")
+    List<Persons> findByCityOfLiving(@Param("city") String city);
 
     List<Persons> findByPrimaryKeyForPersonsAgeLessThan(int age, Sort sort);
 
