325c45f0ef072b905fd4a957e646a1e29c7c14da
Kiva Aleksey <adolfp@mail.ru>
2021-11-03 11:39:55 +0700

    Added @EnableGlobalMethodSecurity

--- src/main/java/ru/netology/hibernate/config/SecurityConfig.java
+++ src/main/java/ru/netology/hibernate/config/SecurityConfig.java
@@ -2,14 +2,18 @@ package ru.netology.hibernate.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("USER");
+        auth.inMemoryAuthentication().withUser("user").password("{noop}1234").roles("READ")
+                .and().withUser("operator").password("{noop}1111").roles("WRITE")
+                .and().withUser("master").password("{noop}2222").roles("DELETE");
     }
 
     @Override
