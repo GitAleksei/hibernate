4fbc90615c093c45e802dfdc4c8032983c1fa18f
Kiva Aleksey <adolfp@mail.ru>
2021-11-03 11:34:40 +0700

    Added SecurityController.java

---
+++ src/main/java/ru/netology/hibernate/controller/SecurityController.java
@@ -0,0 +1,39 @@
+package ru.netology.hibernate.controller;
+
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SecurityController {
+
+    @GetMapping("/role-read")
+    @Secured("ROLE_READ")
+    public String getRead() {
+        return "Hi, " + SecurityContextHolder.getContext().getAuthentication().getName() + " - "
+                + SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+    }
+
+    @GetMapping("/role-write")
+    @Secured("ROLE_WRITE")
+    public String getWrite() {
+        return "Hi, " + SecurityContextHolder.getContext().getAuthentication().getName() + " - "
+                + SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+    }
+
+    @GetMapping("/role-write-or-delete")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
+    public String getWriteOrDelete() {
+        return "Hi, " + SecurityContextHolder.getContext().getAuthentication().getName() + " - "
+                + SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+    }
+
+    @GetMapping("/role-username")
+    @PreAuthorize("#username == authentication.principal.username")
+    public String getUsername(String username) {
+        return "Hi, " + username + " - "
+                + SecurityContextHolder.getContext().getAuthentication().getAuthorities();
+    }
+}
